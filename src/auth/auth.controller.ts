138ac4595d4898a01906e01eccfc8c86b5dbfb7b
Vietanh <vietanhcao1994@gmail.com>
2021-05-09 22:44:03 +0700

    add custom decorator user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,42 @@
-import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  async signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  async signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
 
   @Post('/signin')
-  async signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+  async signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req){
-    console.log(req)
-  }
+  // @Post('/test')
+  // @UseGuards(AuthGuard())
+  // test(
+  //   // @Req() req
+  //   @GetUser() user: User
+  //   ){
+  //   // console.log(req)
+  //   console.log(user)
+  // }
 }
