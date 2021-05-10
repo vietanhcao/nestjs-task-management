270a4563bbb5d62cde7a08179a767e8c86dc0f65
Vietanh <vietanhcao1994@gmail.com>
2021-05-10 22:24:57 +0700

    add relation manytoone 2 task

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,11 +2,13 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 
 import * as bcrypt from 'bcrypt';
+import { Task } from 'src/tasks/task.entity';
 
 @Entity()
 @Unique(['username']) //add contrain
@@ -23,10 +25,13 @@ export class User extends BaseEntity {
   @Column()
   salt: string;
 
+  @OneToMany((type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
+
   validatePassword(password: string): Promise<boolean> {
     // const hash = await bcrypt.hash(password, this.salt);
 
-    // return hash === this.password;    
+    // return hash === this.password;
 
     return bcrypt.compare(password, this.password);
   }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { TaskStatus } from "./task.status.enum";
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Task extends BaseEntity {
@@ -13,5 +14,8 @@ export class Task extends BaseEntity {
   description: string;
 
   @Column()
-  status: TaskStatus
+  status: TaskStatus;
+
+  @ManyToOne(type => User, user => user.tasks, { eager: false})
+  user: User
 }
