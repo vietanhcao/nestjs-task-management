d738707e6f4a046e7f673544a914b92d0ce8db1e
Vietanh <vietanhcao1994@gmail.com>
2021-05-13 17:46:50 +0700

    enableCors for development

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as config from 'config'
+import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig: any = config.get('server')
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const serverConfig: any = config.get('server');
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
-  logger.log('Application listening  on port ' + port)
+  logger.log('Application listening  on port ' + port);
 }
 bootstrap();
